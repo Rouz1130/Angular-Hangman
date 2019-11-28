c4cea9f12dc49c3e644285395ae1079130cbaabd
rouzbeh.majlessi@avanade.com <rouzbeh.majlessi@avanade.com>
2019-11-28 02:07:27 -0800

    test to see if the words are being used correctly, fix some variable names

--- src/app/hangman/hangman.component.ts
+++ src/app/hangman/hangman.component.ts
@@ -7,60 +7,90 @@ import { words } from './words';
   styleUrls: ['./hangman.component.scss']
 })
 export class HangmanComponent implements OnInit {
-
   word = '';
   hiddenWord = '';
-  guesses = 0;
-  won = false;
+  tries = 0;
+  win = false;
   lost = false;
   letters = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-];
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
 
-  constructor() {
-    const randomNumber = Math.floor(Math.random() * words.length);
+  constructor() {}
+
+  ngOnInit() {
+    var randomNumber = Math.floor(Math.random() * words.length);
     this.word = words[randomNumber];
-    this.hiddenWord = '_  '.repeat(this.word.length);
+    this.hiddenWord = '_ '.repeat(this.word.length);
   }
 
-  checkWord() {
+  check(letter: string) {
+    if (document.getElementById('btn-' + letter) as HTMLInputElement === null) {
+      return true;
+    }
+    if (
+      (document.getElementById('btn-' + letter) as HTMLInputElement).disabled ===
+      true
+    ) {
+      return true;
+    }
+    ((
+      document.getElementById('btn-' + letter)
+    ) as HTMLInputElement).disabled = true;
+    if (this.word.indexOf(letter) === -1) {
+      this.tries++;
+    }
+    const hiddenWordArray = this.hiddenWord.split(' ');
+    for (let i = 0; i < this.word.length; i++) {
+      if (this.word[i] === letter) {
+        hiddenWordArray[i] = letter;
+      }
+    }
+    this.hiddenWord = hiddenWordArray.join(' ');
+    this.checkGame();
+  }
+
+  checkGame() {
     const wordArray = this.hiddenWord.split(' ');
     const wordCheck = wordArray.join('');
     if (wordCheck === this.word) {
-        this.won = true;
+      this.win = true;
     }
-    if (this.guesses >= 10) {
-        this.lost = true;
+    if (this.tries >= 10) {
+      this.lost = true;
     }
-}
-
-check(letter: string) {
-  if ((document.getElementById('btn-' + letter) as HTMLInputElement) === null) {
-      return true;
-  }
-  if ((document.getElementById('btn-' + letter) as HTMLInputElement).disabled === true) {
-      return true;
-  }
-  (document.getElementById('btn-' + letter) as HTMLInputElement).disabled = true;
-  if (this.word.indexOf(letter) === -1) {
-      this.guesses++;
-  }
-  const hiddenWordArray = this.hiddenWord.split(' ');
-  for (let i = 0; i < this.word.length; i++) {
-      if (this.word[i] === letter) {
-          hiddenWordArray[i] = letter;
-      }
-  }
-  this.hiddenWord = hiddenWordArray.join(' ');
-  this.checkWord();
-}
-
-onKey(event: KeyboardEvent) {
-  this.check(event.key.toUpperCase());
-}
-
-  ngOnInit() {
   }
 
+  reload() {
+    location.reload();
+  }
+
+  onKey(event: KeyboardEvent) {
+    this.check(event.key.toUpperCase());
+  }
 }
