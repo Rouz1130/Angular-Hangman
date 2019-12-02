cb5ecdb5df156ebc34c023763737fc3cf8f6dc6b
Rouzbeh Majlessi <rouz@Rouzs-MacBook-Pro.local>
2019-12-01 17:49:36 -0800

    fix styling of page clean up some code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+// 3rd party modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+
+// Components
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HangmanComponent } from './hangman/hangman.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    HangmanComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavComponent,
+        HangmanComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
